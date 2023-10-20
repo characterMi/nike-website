e6c2db7774bd2b6e45b544bae194fb9d87af70c4
characterMi <abol1385fx@gmail.com>
2023-10-20 19:09:04 +0330

    style: change the hero and nav styles

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -6,7 +6,7 @@ import { useState } from "react"
 const Nav = () => {
   const [toggle, setToggle] = useState(false)
   return (
-    <header className="padding-x py-8 absolute z-10 w-full">
+    <header className="padding-x py-8 absolute z-50 w-full">
       <nav className="flex justify-between items-center max-container">
         <a href="/">
           <img src={headerLogo} alt="Logo" width={130} height={29} />
